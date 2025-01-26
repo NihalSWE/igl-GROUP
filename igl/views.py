3b3b43133c82fe60aa67be6f2bfdf4075735bdba
NihalSWE <mahamudur35-2563@diu.edu.bd>
2025-01-26 16:16:27 +0600

    connect home,about,contact page

--- igl/views.py
+++ igl/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def base(request):
+    return render(request, 'frontend/base.html')
+
+def home(request):
+    return render(request, 'frontend/home.html')
+
+
+def about(request):
+    return render(request, 'frontend/about.html')
+
+def contact(request):
+    return render(request, 'frontend/contact.html')
+
